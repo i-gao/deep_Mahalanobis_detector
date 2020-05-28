9795d7e2e8d50421dc23ea4a6e7004f94a66ac8f
i-gao <igao@imsa.edu>
2020-05-27 18:58:53 -0700

    densenet patch

--- densenet.py
+++ densenet.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 import math
 import torch
 import torch.nn as nn
@@ -59,7 +66,7 @@ class DenseBlock(nn.Module):
         self.layer = self._make_layer(block, in_planes, growth_rate, nb_layers, dropRate)
     def _make_layer(self, block, in_planes, growth_rate, nb_layers, dropRate):
         layers = []
-        for i in range(int(nb_layers)):
+        for i in range(nb_layers):
             layers.append(block(in_planes+i*growth_rate, growth_rate, dropRate))
         return nn.Sequential(*layers)
     def forward(self, x):
@@ -67,12 +74,13 @@ class DenseBlock(nn.Module):
 
 class DenseNet3(nn.Module):
     def __init__(self, depth, num_classes, growth_rate=12,
-                 reduction=0.5, bottleneck=True, dropRate=0.0):
+                 reduction=0.5, bottleneck=True, dropRate=0.0, normalizer = None):
         super(DenseNet3, self).__init__()
+
         in_planes = 2 * growth_rate
         n = (depth - 4) / 3
         if bottleneck == True:
-            n = n/2
+            n = int(n/2)
             block = BottleneckBlock
         else:
             block = BasicBlock
@@ -97,18 +105,24 @@ class DenseNet3(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.fc = nn.Linear(in_planes, num_classes)
         self.in_planes = in_planes
+        self.normalizer = normalizer
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
                 m.bias.data.zero_()
-                    
+
     def forward(self, x):
+        if self.normalizer is not None:
+            x = x.clone()
+            for idx, inp in enumerate(x):
+                x[idx] = self.normalizer(inp)
+            x = x.cuda()
+
         out = self.conv1(x)
         out = self.trans1(self.block1(out))
         out = self.trans2(self.block2(out))
@@ -116,10 +130,17 @@ class DenseNet3(nn.Module):
         out = self.relu(self.bn1(out))
         out = F.avg_pool2d(out, 8)
         out = out.view(-1, self.in_planes)
-        return self.fc(out)
-    
+        out = self.fc(out)
+        return out
+
     # function to extact the multiple features
     def feature_list(self, x):
+        if self.normalizer is not None:
+            x = x.clone()
+            for idx, inp in enumerate(x):
+                x[idx] = self.normalizer(inp)
+            x = x.cuda()
+
         out_list = []
         out = self.conv1(x)
         out_list.append(out)
@@ -132,16 +153,22 @@ class DenseNet3(nn.Module):
         out_list.append(out)
         out = F.avg_pool2d(out, 8)
         out = out.view(-1, self.in_planes)
-        
+
         return self.fc(out), out_list
-    
+
     def intermediate_forward(self, x, layer_index):
+        if self.normalizer is not None:
+            x = x.clone()
+            for idx, inp in enumerate(x):
+                x[idx] = self.normalizer(inp)
+            x = x.cuda()
+
         out = self.conv1(x)
         if layer_index == 1:
-            out = self.trans1(self.block1(out)) 
+            out = self.trans1(self.block1(out))
         elif layer_index == 2:
             out = self.trans1(self.block1(out))
-            out = self.trans2(self.block2(out))                
+            out = self.trans2(self.block2(out))
         elif layer_index == 3:
             out = self.trans1(self.block1(out))
             out = self.trans2(self.block2(out))
@@ -151,6 +178,12 @@ class DenseNet3(nn.Module):
 
     # function to extact the penultimate features
     def penultimate_forward(self, x):
+        if self.normalizer is not None:
+            x = x.clone()
+            for idx, inp in enumerate(x):
+                x[idx] = self.normalizer(inp)
+            x = x.cuda()
+
         out = self.conv1(x)
         out = self.trans1(self.block1(out))
         out = self.trans2(self.block2(out))
